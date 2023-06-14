628a573c127d13f06eb7538aafc2f7da89112b1b
Historia Montague <historia.montague@outlook.com>
2023-06-14 11:48:34 -0600

    Finished Lab

--- server/app.py
+++ server/app.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python3
 
 from flask import Flask, make_response, jsonify
+from flask_sqlalchemy import SQLAlchemy
 from flask_migrate import Migrate
 
 from models import db, Bakery, BakedGood
@@ -13,26 +14,38 @@ app.json.compact = False
 migrate = Migrate(app, db)
 
 db.init_app(app)
-
+        
 @app.route('/')
 def index():
     return '<h1>Bakery GET API</h1>'
 
 @app.route('/bakeries')
 def bakeries():
-    return ''
+    
+    return make_response(
+        [bakery.to_dict() for bakery in Bakery.query.all()],
+        200)
 
 @app.route('/bakeries/<int:id>')
 def bakery_by_id(id):
-    return ''
+    return make_response(
+        Bakery.query.filter(Bakery.id == id).first().to_dict(),
+        200
+    )
 
 @app.route('/baked_goods/by_price')
 def baked_goods_by_price():
-    return ''
+    return make_response(
+        [baked_good.to_dict() for baked_good in  BakedGood.query.order_by(BakedGood.price.desc()).all()],
+        200
+    )
 
 @app.route('/baked_goods/most_expensive')
 def most_expensive_baked_good():
-    return ''
+    return make_response(
+        BakedGood.query.order_by(BakedGood.price.desc()).first().to_dict(),
+        200
+    )
 
 if __name__ == '__main__':
-    app.run(port=555, debug=True)
+    app.run(port=5555, debug=True)
